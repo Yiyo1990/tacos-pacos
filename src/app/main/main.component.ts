8458e08e104f703642da59108681ae72da855ca2
Maurilio Lopez Martinez <maurilio.lopez02@contractor.metalsa.com>
2023-09-05 21:49:55 -0600

    Se esta agregando modulo de gastos

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MainService } from './main.service';
 
 @Component({
   selector: 'app-main',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  currentPage: string = ""
 
+  constructor(private service: MainService) {
+    service.getFoodCategories()
+    service.getProvidersCategories()
+    service.getOperationsCategories()
+    service.$pageName.subscribe((name: string) => {
+      this.currentPage = name
+    })
+  }
 }
---
+++ src/app/main/main.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { BehaviorSubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MainService {
+
+  private endPoint: string = environment.apiUrl
+
+  private pageName = new BehaviorSubject("Inicio")
+  $pageName = this.pageName.asObservable()
+
+  private providersCategories = new BehaviorSubject(null)
+  $providersCategories = this.providersCategories.asObservable();
+
+  private foodCategories = new BehaviorSubject(null)
+  $foodCategories = this.foodCategories.asObservable();
+
+  private operationCategories = new BehaviorSubject(null)
+  $operationCategories = this.operationCategories.asObservable();
+
+
+  constructor(private http: HttpClient) {}
+
+  setPageName(name: string) {
+    this.pageName.next(name)
+  }
+
+  async getProvidersCategories() {
+    let url = this.endPoint.concat('expense/provider-categories')
+    this.http.get(url).subscribe({
+      next: (result: any) => {
+        this.providersCategories.next(result)
+      },
+      error: (error) => {
+        this.providersCategories.next(error)
+      }
+    })
+  }
+
+  async getFoodCategories() {
+    let url = this.endPoint.concat('expense/food-categories')
+    this.http.get(url).subscribe({
+      next: (result: any) => {
+        this.foodCategories.next(result)
+      },
+      error: (error) => {
+        this.foodCategories.next(error)
+      }
+    })
+  }
+
+  async getOperationsCategories() {
+    let url = this.endPoint.concat('expense/operations-categories')
+    this.http.get(url).subscribe({
+      next: (result: any) => {
+        this.operationCategories.next(result)
+      },
+      error: (error) => {
+        this.operationCategories.next(error)
+      }
+    })
+  }
+}
