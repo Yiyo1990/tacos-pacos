2ba2398e081044ec9cd1a40105f1f2304a728bd7
Maurilio Lopez Martinez <maurilio.lopez02@contractor.metalsa.com>
2024-01-23 00:02:29 -0600

    Se agrega grafica venta diaria y tabla tipo de pago

--- src/app/util/Charts.ts
+++ src/app/util/Charts.ts
@@ -1,5 +1,6 @@
 
 export class Charts {
+  public static chartColors = { general: '#2b65ab', dinningRoom: "#3889EB", uber: "#31B968", rappi: "#F31A86", didi: "#F37D1A" }
 
   public static Donut(labels: string[], data: any[], colors: any[]): any {
 
