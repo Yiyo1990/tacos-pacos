367e6af07129ce123c74dde8cebfc536ce9971ef
Maurilio Lopez Martinez <maurilio.lopez02@contractor.metalsa.com>
2024-02-18 14:34:58 -0600

    Se rediseña proveedores

--- src/app/components/category-supplier-card/category-supplier-card.component.ts
+++ src/app/components/category-supplier-card/category-supplier-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'category-supplier-card',
@@ -9,4 +9,17 @@ export class CategorySupplierCardComponent {
   @Input() isEditable: boolean = false
   @Input() providers: Array<any> = []
   @Input() nameCategory: string = ""
+  @Output() onActionEvent: EventEmitter<any> = new EventEmitter()
+  @Output() onAddCategoryEvent: EventEmitter<any> = new EventEmitter()
+
+
+  onClickActions(type: number, item: any) {
+    this.onActionEvent.emit({type: type, item: item})
+  }
+
+  onClickAddCategory() {
+    this.onAddCategoryEvent.emit()
+  }
 }
+
+
